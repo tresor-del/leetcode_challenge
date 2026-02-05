fd66ce6efdbc86cf7fde7d0e27e6fe328126d8fb
tresor-del <tresoradzanto7@gmail.com>
2026-02-05 22:37:45 +0000

    day2: Consecutive Ones

---
+++ day2/Solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        int maxCount = 0;
+        int current = 0;
+
+        for (int i = 0; i < nums.length; i++){
+
+            if (nums[i] == 0) {
+
+                if (maxCount > current) {
+                    current = maxCount;
+                }
+                maxCount = 0;
+            }
+            if (nums[i] == 1) {
+                current ++;
+            }
+        }
+
+        if (current < maxCount) {
+            return maxCount;
+        } else {
+            return current;
+        }
+
+    }
+}
